444f96f4ce89b95edd7d9e2581297247bffc7762
kalettuce <kjiangzk7@gmail.com>
2021-01-30 18:17:09 -0800

    Example database operation added to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import firebase from "firebase";
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
@@ -6,8 +7,21 @@ import Logout from './components/Logout';
 
 function App() {
 
+  // a reference to the database
+  var database = firebase.database();
+
+
   const [loggedIn, setLoggedIn] = useState(false)
   const [name, setName] = useState()
+
+	function testfunc() {
+    console.log("entered");
+		database.ref("tests/test1").set( {
+			testnum : 1,
+			really : true,
+			yay : "no"
+		});
+	}
 
   return (
     <div className="21 days">
@@ -17,6 +31,8 @@ function App() {
           Welcome to 21 days, a habit tracker.
         </p>
       </header>
+
+			<button onClick={testfunc}>test button</button>
 
       <div style={{}}>
         <Login loggedIn={loggedIn} setLoggedIn = {(bool) => setLoggedIn(bool)} setName={(name) => setName(name)}/>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from "firebase/app"
 import reportWebVitals from './reportWebVitals';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB9bW7Re6B3y3Bv7BViWNWvPquq4gHkoUg",
+  authDomain: "knac-app.firebaseapp.com",
+  databaseURL: "https://knac-app-default-rtdb.firebaseio.com",
+  projectId: "knac-app",
+  storageBucket: "knac-app.appspot.com",
+  messagingSenderId: "608833851290",
+  appId: "1:608833851290:web:aef40085e4293f81b23423",
+  measurementId: "G-JHW6KPM0L7"
+});
 
 ReactDOM.render(
   <React.StrictMode>
