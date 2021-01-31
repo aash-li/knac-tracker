32e092b3606d09ae2e692ca7e9a4e7b1d72f0bef
awliao <ashley.w.liao@gmail.com>
2021-01-30 21:40:58 -1000

    Added multi-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
+import Progress from './components/Progress';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header, Home, About, Contact } from "./components";
 
 function App() {
 
@@ -25,15 +28,17 @@ function App() {
 
   return (
     <div className="21 days">
-      <header className="21 Days">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to 21 days, a habit tracker.
-        </p>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/about" exact component={() => <About />} />
+          <Route path="/contact" exact component={() => <Contact />} />
+        </Switch>
+      </Router>
+      <div><Progress /></div>
 
 			<button onClick={testfunc}>test button</button>
-
       <div style={{}}>
         <Login loggedIn={loggedIn} setLoggedIn = {(bool) => setLoggedIn(bool)} setName={(name) => setName(name)}/>
         {loggedIn ? <p>Hello {name}</p>: <p>Not logged in</p> }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
