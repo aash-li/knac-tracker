85cf7a24c8c38baa444b3b2a63e6a7b317951c4f
aash-li <ashley.w.liao@gmail.com>
2021-08-15 13:57:03 -0700

    added initial for goal display

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, {Component} from "react";
 import Habit from './Habit';
 import firebase from "firebase";
 
-function Goal(props) {
-  return (
-    <div className="goal">
-      <div class="container">
-        <div class="row align-items-center my-5">
-        <Habit id='1' userId={props.userId} database={firebase.database()}/>
+// props: userid, database
+class Goal extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {}
+    }
+  }
+
+  componentDidMount() {
+    let numHabitsRef = this.props.database.ref("userToHabit/" + this.props.userId + "/numHabits")
+    let numHabits;
+    numHabitsRef.on('value', (snapshot) => {
+      numHabits = snapshot.val();
+    })
+    console.log("here are teh number of habits")
+    console.log(numHabits)
+    console.log(this.props.userId)
+    let allIds = {};
+    for (var i = 0; i < numHabits; i++) {
+      let curRef = this.props.database.ref("userToHabit/this.props.userId/" + i + "/HabitId")
+      let curId;
+      curRef.on('value', (snapshot) => {
+        curId = snapshot.val();
+      })
+      console.log("getting an id here")
+      console.log(curId)
+      allIds.add(curId)
+    }
+    this.setState({
+      options: allIds
+    })
+    console.log("these are the ids..")
+    console.log(this.state.options)
+  }
+
+  render() {
+    var options = [];
+    options.push(<option value={"null"}>Select a Habit</option>)
+    for (var i = 0; i < this.state.options.length; i++) {
+      options.push(<option value={this.state.options[i]}>{this.state.options[i]}</option>)
+    }
+    return (
+      <div className="goal">
+        <div class="container">
+          <div class="row align-items-center my-5">
+          <select>{options}</select>
+          <Habit id='1' userId={this.props.userId} database={firebase.database()}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Goal;
